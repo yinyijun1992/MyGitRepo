2bc74ca899195711c39942d0ea65074ee5290708
yinyijun <yinyijun1992@hotmail.com>
2016-09-28 21:10:44 +0800

    -m add "modelAExample.py"

---
+++ MyPython/modelAExample.py
@@ -0,0 +1,8 @@
+"""
+modelAExample.py
+
+A simple example: Creating a part.
+"""
+from abaqus import *
+from abaqusConstants import *
+backwardCompatibility.setValues(includeDeprecated=True, reportDeprecated=False)
